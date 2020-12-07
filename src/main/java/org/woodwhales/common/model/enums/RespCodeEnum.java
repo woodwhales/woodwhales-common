26381b7e3c1db7876d653d3415bd21ee8510ddeb
woodwhales <woodwhales@163.com>
2020-12-07 14:17:52 +0800

    added interface for CodeResp and updated version to 1.4.0

--- src/main/java/org/woodwhales/common/model/enums/RespCodeEnum.java
+++ src/main/java/org/woodwhales/common/model/enums/RespCodeEnum.java
@@ -1,23 +1,24 @@
 package org.woodwhales.common.model.enums;
 
 import lombok.Getter;
+import org.woodwhales.common.model.result.BaseRespResult;
 
 /**
  * @author woodwhales on 2020-08-25
  * @description 通用响应枚举
  */
 @Getter
-public enum RespCodeEnum {
+public enum RespCodeEnum implements BaseRespResult {
 
     /**
      * 响应成功
      */
-    SUCCESS(0, "success"),
+    SUCCESS(0, "操作成功"),
 
     /**
      * 响应失败
      */
-    ERROR(-1, "fail"),
+    ERROR(-1, "操作失败"),
     ;
 
     /**
@@ -35,4 +36,8 @@ public enum RespCodeEnum {
         this.message = message;
     }
 
+    @Override
+    public boolean isSucceed() {
+        return this.code == 0;
+    }
 }
---
+++ src/main/java/org/woodwhales/common/model/result/BaseRespResult.java
@@ -0,0 +1,27 @@
+package org.woodwhales.common.model.result;
+
+/**
+ * @author woodwhales on 2020-12-07
+ * @description 通用响应报文接口
+ */
+public interface BaseRespResult {
+
+    /**
+     * 是否成功
+     * @return
+     */
+    boolean isSucceed();
+
+    /**
+     * 获取响应描述
+     * @return
+     */
+    String getMessage();
+
+    /**
+     * 获取响应状态码
+     * @return
+     */
+    int getCode();
+
+}
