a619a2acb6c01f348aad1117c1b86687c29af14a
woodwhales <woodwhales@163.com>
2021-06-15 22:40:45 +0800

    updated PageQueryParam

--- src/main/java/org/woodwhales/common/model/param/PageQueryParam.java
+++ src/main/java/org/woodwhales/common/model/param/PageQueryParam.java
@@ -4,6 +4,7 @@ import lombok.Data;
 import org.woodwhales.common.model.field.PageQueryInterface;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 /**
  * @author woodwhales on 2020-08-25
@@ -16,11 +17,13 @@ public class PageQueryParam implements PageQueryInterface {
      * 当前页码
      */
     @Min(value = 1, message = "当前页码不允许小于第一页")
+    @NotNull(message = "page不允许为空")
     private Integer page;
 
     /**
      * 每页显示总记录数
      */
     @Min(value = 0, message = "当前页总记录数不允许为负数")
+    @NotNull(message = "limit不允许为空")
     private Integer limit;
 }
