e70a5e92821f30aebaafd67072bf48f8dbd51271
woodwhales <woodwhales@163.com>
2023-04-16 22:34:35 +0800

    update RequestAspectTool

--- src/main/java/cn/woodwhales/common/util/RequestAspectTool.java
+++ src/main/java/cn/woodwhales/common/util/RequestAspectTool.java
@@ -18,8 +18,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import reactor.core.publisher.Mono;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.*;
@@ -36,6 +38,11 @@ import java.util.stream.Collectors;
 public class RequestAspectTool {
 
     /**
+     * 请求 traceId
+     */
+    public static final String TRACE_ID = "TRACE_ID";
+
+    /**
      * 对请求controller进行切面
      * 使用uuid作为请求流水号，请求之前将请求流水号设置为当前线程名称
      *
@@ -47,108 +54,141 @@ public class RequestAspectTool {
         return aspect(joinPoint,
                 () -> UUID.randomUUID().toString(true),
                 request -> {
-                    request.setAttribute("traceId", Thread.currentThread().getName());
-                }, null);
+                    request.setAttribute(TRACE_ID, Thread.currentThread().getName());
+                }, null, null);
     }
 
     /**
      * 对请求controller进行切面
      *
      * @param joinPoint         joinPoint
-     * @param requestIdSupplier 请求流水号生成接口
-     * @param beforeConsumer    请求执行之前的接口
-     * @param afterConsumer     请求执行之后的接口
+     * @param traceIdSupplier 请求流水号生成接口
+     * @param beforeRequestConsumer    请求执行之前的接口
+     * @param afterResponseConsumer     请求执行之后的接口
      * @return 请求执行结果
      * @throws Throwable 异常对象
      */
     public static Object aspect(ProceedingJoinPoint joinPoint,
-                                Supplier<String> requestIdSupplier,
-                                Consumer<HttpServletRequest> beforeConsumer,
-                                BiConsumer<HttpServletRequest, Object> afterConsumer) throws Throwable {
-        // 将当前线程名称改为 UUID
-        if (Objects.nonNull(requestIdSupplier)) {
-            String requestId = requestIdSupplier.get();
-            if (StringUtils.isNotBlank(requestId)) {
-                Thread.currentThread().setName(requestId);
-            }
-        }
-
+                                Supplier<String> traceIdSupplier,
+                                Consumer<HttpServletRequest> beforeRequestConsumer,
+                                BiConsumer<HttpServletRequest, Object> afterResponseConsumer,
+                                Consumer<RequestDto> finalConsumer) throws Throwable {
+        RequestDto requestDto = new RequestDto();
         StopWatch stopWatch = new StopWatch();
-        stopWatch.start();
-        Signature signature = joinPoint.getSignature();
-        String declaringTypeName = signature.getDeclaringType()
-                .getName();
-        String fullMethodName = declaringTypeName + "#" + signature.getName();
-        StringBuilder requestParamBuilder = new StringBuilder();
-        String clientIpAddress;
-        String requestUrl = null;
-        HttpServletRequest request;
+        String fullMethodName = null;
         try {
+            String traceId = null;
+            // 获取 traceId 并设置到当前线程名中
+            if (Objects.nonNull(traceIdSupplier)) {
+                traceId = traceIdSupplier.get();
+                if (StringUtils.isBlank(traceId)) {
+                    traceId = UUID.randomUUID().toString();
+                }
+            } else {
+                traceId = UUID.randomUUID().toString();
+            }
+
+            requestDto.traceId = traceId;
+            Thread.currentThread().setName(traceId);
+
+            stopWatch.start();
+            Signature signature = joinPoint.getSignature();
+            String declaringTypeName = signature.getDeclaringType().getName();
+            fullMethodName = declaringTypeName + "#" + signature.getName();
+
             log.info("======================= {} start =======================", fullMethodName);
-            request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
-            clientIpAddress = IpTool.getIpAddress(request);
-            String methodType = request.getMethod();
-            requestUrl = String.format("[ %s ] %s", methodType, request.getRequestURL()
-                    .toString());
-            // 请求地址
-            log.info("request_url={}", requestUrl);
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            HttpServletResponse response = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getResponse();
             // 请求ip
-            log.info("request_ip={}", clientIpAddress);
+            requestDto.clientIpAddress = IpTool.getIpAddress(request);
+
+            // 请求url
+            String methodType = request.getMethod();
+            requestDto.requestUrl = String.format("[ %s ] %s", methodType, request.getRequestURL().toString());
+
             // 请求参数
-            log.info("request_param={}", requestParamBuilder.append(getParamStringFromRequest(request)));
+            requestDto.requestParam = getParamStringFromRequest(request);
+
+            // 请求报文
             if (!StringUtils.equals(RequestMethod.GET.name(), methodType)) {
                 Object requestBodyParam = collectRequestBodyParam(joinPoint.getArgs(), signature);
-                if (null != requestBodyParam) {
-                    log.info("request_requestBody={}", requestParamBuilder.append(JsonTool.toJSONString(requestBodyParam)));
-                }
+                requestDto.requestBody = JsonTool.toJSONString(requestBodyParam);
             }
             //请求头信息
-            if (Objects.nonNull(beforeConsumer)) {
-                beforeConsumer.accept(request);
+            if (Objects.nonNull(beforeRequestConsumer)) {
+                beforeRequestConsumer.accept(request);
             }
+
+            log.info("请求url：{}", requestDto.requestUrl);
+            log.info("请求ip：{}", requestDto.clientIpAddress);
+            log.info("请求参数：{}", requestDto.requestParam);
+
             Object result = joinPoint.proceed();
-            printResponseBody(result);
-            if (Objects.nonNull(afterConsumer)) {
-                afterConsumer.accept(request, result);
+
+            // 响应报文
+            printResponseBody(result, requestDto);
+
+            log.info("响应报文：{}", requestDto.requestBody);
+            if (Objects.nonNull(afterResponseConsumer)) {
+                afterResponseConsumer.accept(request, result);
             }
+            // 响应头中增加 traceId
+            response.addHeader(TRACE_ID, requestDto.traceId);
             return result;
         } catch (Throwable throwable) {
-            String requestParam = requestParamBuilder.toString();
-            log.error("system happen error {}, requestParam : {}", requestUrl, requestParam, throwable);
+            log.error("请求响应异常, errorMsg={}, 请求url：{}, 请求参数：{}, 请求报文：{}",
+                    throwable.getMessage(), requestDto.requestUrl, requestDto.requestParam, requestDto.requestBody, throwable);
+            requestDto.throwable = throwable;
             throw throwable;
         } finally {
-            long costTime = stopWatch.getTotalTimeMillis();
-            log.info("requestUrl = {}, consume : {} ms", requestUrl, costTime);
+            requestDto.costTime = stopWatch.getTotalTimeMillis();
+            log.info("请求url：{}, 响应耗时：{} ms", requestDto.requestUrl, requestDto.costTime);
+            if(Objects.nonNull(finalConsumer)) {
+                finalConsumer.accept(requestDto);
+            }
             log.info("======================= {} end =======================", fullMethodName);
         }
     }
 
-    private static void printResponseBody(Object result) {
+    private static void printResponseBody(Object result, RequestDto requestDto) {
         if(Objects.isNull(result)) {
-            log.info("request_responseBody={}", "响应报文为空");
-            return;
+            requestDto.requestBody = null;
         }
 
-        // 文件流不打印响应报文
-        if (result instanceof ResponseEntity) {
+        if(result instanceof Mono) {
+            (((Mono)result)).subscribe(resp -> {
+                if(resp instanceof ResponseEntity) {
+                    ResponseEntity responseEntity = (ResponseEntity) resp;
+                    HttpHeaders headers = responseEntity.getHeaders();
+                    MediaType contentType = headers.getContentType();
+                    if(Objects.nonNull(contentType) && MediaType.APPLICATION_OCTET_STREAM.getType().equals(contentType.getType())) {
+                        requestDto.requestBody = "文件流不打印响应日志";
+                    } else {
+                        requestDto.requestBody = JsonUtils.toJson(responseEntity.getBody());
+                    }
+                } else {
+                    requestDto.requestBody = JsonUtils.toJson(resp);
+                }
+            });
+        } else if (result instanceof ResponseEntity) {
             ResponseEntity<?> responseEntity = (ResponseEntity<?>) result;
             HttpHeaders headers = responseEntity.getHeaders();
             MediaType contentType = headers.getContentType();
+            // 文件流不打印响应报文
             if (Objects.nonNull(contentType) && MediaType.APPLICATION_OCTET_STREAM.getType().equals(contentType.getType())) {
-                log.info("request_responseBody={}", "响应报文为文件流, 不打印响应报文");
+                requestDto.requestBody = "响应报文为文件流, 不打印响应报文";
+                return;
             } else {
-                log.info("request_responseBody={}", JsonTool.toJSONString(result));
+                requestDto.requestBody = JsonUtils.toJson(result);
             }
-            return;
         }
 
         // 数组不打印响应报文
         if (result.getClass().isArray()) {
-            log.info("request_responseBody={}", "响应报文为数组, 不打印响应报文");
-            return;
+            requestDto.requestBody = "响应报文为数组, 不打印响应报文";
+        } else {
+            requestDto.requestBody = JsonUtils.toJson(result);
         }
-
-        log.info("request_responseBody={}", JsonTool.toJSONString(result));
     }
 
     private static Object collectRequestBodyParam(Object[] args, Signature signature) {
@@ -179,5 +219,35 @@ public class RequestAspectTool {
                 .join(map);
     }
 
+    public static class RequestDto {
+        /**
+         * 请求ip
+         */
+        String clientIpAddress;
+        /**
+         * 请求url
+         */
+        String requestUrl;
+        /**
+         * traceId
+         */
+        String traceId;
+        /**
+         * 请求参数
+         */
+        String requestParam;
+        /**
+         * 请求报文
+         */
+        String requestBody;
+        /**
+         * 本次请求响应的耗时，单位：ms
+         */
+        long costTime;
+        /**
+         * 本次请求发生异常，如果发生异常，则不为null
+         */
+        Throwable throwable;
+    }
 
 }
